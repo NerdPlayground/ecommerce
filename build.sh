8d012d4e6f984822f1036dbc2bbf96e425c38109
Nerd <97788907+NerdPlayground@users.noreply.github.com>
2024-09-19 23:39:37 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building project packages..."
+pip install -r requirements.txt
+
+echo "Migrating Database..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
