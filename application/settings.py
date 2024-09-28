6ef73ece6c7545974c110b63f10f4b49c6d3a3e0
Nerd <studytime023@gmail.com>
2024-09-28 19:54:08 +0300

    fix (Deployment) : configure hosts and origins

--- application/settings.py
+++ application/settings.py
@@ -29,7 +29,17 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = config('DEBUG',True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "ecommerce-aw0i.onrender.com",
+]
+
+
+# External API Interaction
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ecommerce-aw0i.onrender.com",
+]
 
 
 # Application definition
